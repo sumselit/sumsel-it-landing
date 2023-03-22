1e2b5142cc8f2e6b248df33dfbe92e3c36407181
Aditya Dharmawan Saputra <37553901+adityadees@users.noreply.github.com>
2023-03-22 09:05:57 +0700

    desktop view

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,7 +1,10 @@
-<nav class="navbar navbar-expand-lg navbar-light bg-light">
-	<div class="container-fluid">
-		<a class="navbar-brand" href="#">Navbar</a>
-		<button
+<nav class="navbar navbar-expand-lg   px-5">
+	<div class="container">
+		<a class="navbar-brand" href="/">
+			<img src="../logo/logo.png" alt="" height="48" />
+			SUMSELIT</a
+		>
+		<!-- <button
 			class="navbar-toggler"
 			type="button"
 			data-bs-toggle="collapse"
@@ -11,19 +14,19 @@
 			aria-label="Toggle navigation"
 		>
 			<span class="navbar-toggler-icon" />
-		</button>
-		<div class="collapse navbar-collapse" id="navbarSupportedContent">
+		</button> -->
+		<!-- <div class="collapse navbar-collapse" id="navbarSupportedContent">
 			<ul class="navbar-nav me-auto mb-2 mb-lg-0">
 				<li class="nav-item">
-					<a class="nav-link active" aria-current="page" href="#">Home</a>
+					<a class="nav-link active" aria-current="page" href="/">Home</a>
 				</li>
 				<li class="nav-item">
-					<a class="nav-link" href="#">Link</a>
+					<a class="nav-link" href="/">Link</a>
 				</li>
 				<li class="nav-item dropdown">
 					<a
 						class="nav-link dropdown-toggle"
-						href="#"
+						href="/"
 						id="navbarDropdown"
 						role="button"
 						data-bs-toggle="dropdown"
@@ -32,20 +35,209 @@
 						Dropdown
 					</a>
 					<ul class="dropdown-menu" aria-labelledby="navbarDropdown">
-						<li><a class="dropdown-item" href="#">Action</a></li>
-						<li><a class="dropdown-item" href="#">Another action</a></li>
+						<li><a class="dropdown-item" href="/">Action</a></li>
+						<li><a class="dropdown-item" href="/">Another action</a></li>
 						<li><hr class="dropdown-divider" /></li>
-						<li><a class="dropdown-item" href="#">Something else here</a></li>
+						<li><a class="dropdown-item" href="/">Something else here</a></li>
 					</ul>
 				</li>
 				<li class="nav-item">
-					<a class="nav-link disabled">Disabled</a>
+					<a class="nav-link disabled" href="/">Disabled</a>
 				</li>
 			</ul>
 			<form class="d-flex">
 				<input class="form-control me-2" type="search" placeholder="Search" aria-label="Search" />
 				<button class="btn btn-outline-success" type="submit">Search</button>
 			</form>
-		</div>
+		</div> -->
 	</div>
 </nav>
+<section id="hero-slider" class="bg-gradient px-5">
+	<div class="container">
+		<div class="row d-flex align-items-center">
+			<div class="col-lg-6 col-md-6 col-sm-6 col-xs-6">
+				<p class="sub-title mb-3">Sumsel IT</p>
+				<h1 class="title mb-3"><span class="gradient">Komunitas</span> digital Sumatera Selatan</h1>
+				<p class="description mb-5">
+					Bergabunglah dengan semua penggiat digital untuk memperluas jaringan, belajar, dan
+					berkembang bersama-sama
+				</p>
+				<a href="/" class="btn btn-primary btn-large">Gabung Sekarang </a>
+			</div>
+			<div class="col-lg-6 col-md-6 col-sm-6 col-xs-6 text-end">
+				<img src="../images/image-1.png" class="img-fluid" alt="" />
+			</div>
+		</div>
+	</div>
+</section>
+
+<section id="section-info" class="p-5">
+	<div class="container">
+		<div class="card bg-dark p-5">
+			<div class="card-body">
+				<div class="row">
+					<div class="col-lg-12 col-md-12 col-sm-12 col-xs-12 mb-5">
+						<div class="d-md-flex d-sm-block justify-content-between">
+							<div>
+								<span class="sub-title">Keuntungan</span>
+								<h1 class="title">Yang kamu dapat di komuntas ini</h1>
+							</div>
+							<img src="../images/image-2.png" height="161" alt="" />
+						</div>
+					</div>
+					<div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
+						<div class="row">
+							<div class="col-lg-6 col-md-6 col-sm-6 col-xs-6 mb-5">
+								<div class="list">
+									<div class="circle info"><i class="fa fa-link" /></div>
+									<div class="content">
+										<span class="sub-title">Meningkatkan koneksi sosial</span>: Bergabung dengan
+										komunitas digital dapat membantu individu untuk berinteraksi dengan orang-orang
+										yang memiliki minat atau tujuan yang sama. Ini dapat membantu meningkatkan
+										koneksi sosial dan memperluas jaringan teman.
+									</div>
+								</div>
+							</div>
+							<div class="col-lg-6 col-md-6 col-sm-6 col-xs-6 mb-5">
+								<div class="list">
+									<div class="circle warning"><i class="fa fa-link" /></div>
+									<div class="content">
+										<span class="sub-title">Membantu mencari solusi</span>: Komunitas digital juga
+										dapat menjadi tempat untuk mencari solusi atas masalah atau tantangan yang
+										dihadapi. Dengan berdiskusi dengan anggota komunitas yang memiliki pengalaman
+										atau pengetahuan yang relevan, individu dapat memperoleh ide atau saran yang
+										berguna untuk menyelesaikan masalah yang dihadapinya.
+									</div>
+								</div>
+							</div>
+						</div>
+						<div class="row">
+							<div class="col-lg-6 col-md-6 col-sm-6 col-xs-6 mb-5">
+								<div class="list">
+									<div class="circle success"><i class="fa fa-book-open" /></div>
+									<div class="content">
+										<span class="sub-title">Meningkatkan pengetahuan</span>: Komunitas digital
+										sering kali terdiri dari orang-orang yang memiliki pengetahuan dan pengalaman
+										yang berbeda-beda. Bergabung dengan komunitas tersebut dapat membantu individu
+										untuk mempelajari hal baru dan memperluas wawasannya.
+									</div>
+								</div>
+							</div>
+							<div class="col-lg-6 col-md-6 col-sm-6 col-xs-6 mb-5">
+								<div class="list">
+									<div class="circle danger"><i class="fa fa-link" /></div>
+									<div class="content">
+										<span class="sub-title">Menyediakan dukungan dan motivasi</span>: Bergabung
+										dengan komunitas digital juga dapat memberikan dukungan dan motivasi. Ketika
+										individu merasa kesulitan atau kehilangan semangat untuk mencapai tujuannya,
+										anggota komunitas dapat memberikan dukungan moral dan motivasi yang diperlukan.
+									</div>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+</section>
+
+<section id="section-learning" class="px-5">
+	<div class="container p-5 text-center">
+		<div class="row">
+			<div class="col-lg-12 col-md-12 col-sm-12 col-xs-12 mb-5 sub-header">
+				<span class="sub-title">Sumber Belajar</span>
+				<h1 class="title">Sumber belajar digital</h1>
+				<p class="description">
+					Berikut beberapa sumber belajar keahlian dibidang digital bagi kamu yang baru mulai atau
+					bahkan kamu yang sudah terjun langsung didunia digital.
+				</p>
+			</div>
+			<div class="col-lg-12 col-md-12 col-sm-12 col-xs-12 mb-5">
+				<ul class="nav nav-pills nav-fill mb-5">
+					<li class="nav-item">
+						<a class="nav-link active" aria-current="page" href="/">Active</a>
+					</li>
+					<li class="nav-item">
+						<a class="nav-link" href="/">Web Development</a>
+					</li>
+					<li class="nav-item">
+						<a class="nav-link" href="/">Mobile Development</a>
+					</li>
+					<li class="nav-item">
+						<a class="nav-link" href="/">Digital Martketing</a>
+					</li>
+					<li class="nav-item">
+						<a class="nav-link" href="/">IT Security</a>
+					</li>
+					<li class="nav-item">
+						<a class="nav-link" href="/">UI/UX</a>
+					</li>
+				</ul>
+			</div>
+			<div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
+				<h1 class="coming-soon"><span class="gradient">COMING SOON</span></h1>
+			</div>
+		</div>
+	</div>
+</section>
+
+<section id="section-map" class="p-5">
+	<div class="container bg-gradient p-5">
+		<div class="row">
+			<div class="col-lg-8 col-md-8 col-sm-12 col-xs-12 mb-5">
+				<span class="sub-title">Yuk Gabung</span>
+				<h1 class="title">Segera daftarkan dirimu</h1>
+				<p class="description mb-4">
+					Bergabunglah dengan semua penggiat digital untuk memperluas jaringan, belajar, dan
+					berkembang bersama-sama
+				</p>
+				<a href="/" class="btn btn-primary btn-large">Gabung Sekarang </a>
+			</div>
+			<!-- <div class="col-lg-4 col-md-4 col-sm-12 col-xs-12 mb-5">
+				<img src="../images/map.png	" alt="" />
+			</div> -->
+		</div>
+	</div>
+</section>
+
+<section id="footer" class="top bg-dark text-white">
+	<div class="container py-5">
+		<div class="row">
+			<div class="col-lg-8 col-md-8 col-sm-12 col-xs-12">
+				<a href="/" class="logo">
+					<img src="../logo/logo.png" height="52" alt=""/>
+					SUMSELIT
+				</a>
+				<p class="description my-4">
+					<span class="sub-description">SUMSEL IT</span> adalah Wadah penggiat digital Sumatera Selatan adalah sebuah organisasi atau
+					komunitas yang terdiri dari individu-individu yang memiliki minat dan keahlian dalam
+					bidang digital, seperti teknologi informasi, pemasaran digital, desain grafis, dan
+					sebagainya. Tujuan dari wadah ini adalah untuk saling berbagi pengetahuan, pengalaman, dan
+					ide-ide terbaru di bidang digital, serta untuk mengembangkan kemampuan dan kreativitas
+					anggotanya.
+				</p>
+				<p class="contact">+628102909320 - Palembang, Sumatera Selatan</p>
+			</div>
+			<!-- <div class="col-lg-4 col-md-4 col-sm-12 col-xs-12 mb-5">
+				<img src="../images/map.png	" alt="" />
+			</div> -->
+		</div>
+	</div>
+	<footer class="bg-dark-1 p-5">
+		<div class="container">
+			<div class="row">
+				<div class="col-md-12">
+					<div class="d-flex justify-content-between align-items-center">
+						<div>@2023 SUMSEL IT. All rights reserved.</div>
+						<ul>
+							<li><a href="/">Privacy Policy</a></li>
+							<li><a href="/">FAQ</a></li>
+							<li><a href="/">Contact us</a></li>
+						</ul>
+					</div>
+				</div>
+			</div>
+		</div>
+	</footer>
+</section>
